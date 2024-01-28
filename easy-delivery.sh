858bb1ece76c93ea0ec1460ccf5a32ae916deeaa
Soledad <soledadcot@gmail.com>
2024-01-28 16:44:09 +0100

    fix: file for automatic custom domain change in deployment

--- easy-delivery.sh
+++ easy-delivery.sh
@@ -1,7 +1,7 @@
 npm run build
+cp CNAME dist/
 git push origin --delete gh-pages
 git add dist -f 
-git add CNAME
 git commit -m "deployment"
 git subtree push --prefix dist origin gh-pages
 git reset HEAD~ 
