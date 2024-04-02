b4726716c356835023af5da0175d90b83cecbac4
Soledad <soledadcot@gmail.com>
2024-04-02 12:44:08 +0200

    feat: updated timeline as cards

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,71 @@
+<template>
+    <div cols="auto" class="title " :class="{'title-left' : isSideLeft, 'title-right' : !isSideLeft}">
+        <v-container class="card" :class="{'card-l': isSideLeft, 'card-r': !isSideLeft}">
+            <v-row>
+                <v-col>
+                    <h1 class="text-h4">{{ experience.title }}</h1>
+                    <p :class="{'leftParagraph' : isSideLeft, 'rightParagraph' : !isSideLeft}" class="font-weight-light"> {{ experience.environnement }}</p>
+                    <li v-for="desc in experience.description">{{ desc }}</li>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script setup lang="ts">
+//TO DO : MAKE LEFT RIGHT ENUM
+const props = defineProps({
+    experience: {
+        type: Object,
+        default: null
+    },
+    isSideLeft: {
+        type: Boolean
+    }
+}
+);
+</script>
+
+<style scoped>
+.title {
+    color:#414042;
+}
+.title::before {
+    content: "";
+    display: block;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #323b4c;
+}
+
+.title-left::before {
+    float: right;
+    margin-top: 2em;
+    margin-right: -1.095em;
+    margin-left: 1em;
+}
+
+.title-right::before {
+    float: left;
+    margin-right: 10px;
+    margin-top: 1.8em;
+    margin-left: -1.1em;
+  }
+
+.card{
+    padding-bottom: 15em;
+}
+
+.card-l{
+    padding-left:5em;
+}
+
+.card-r{
+    padding-right: 5em;
+}
+
+.rightParagraph {
+    margin-left: 0.5em;
+  }
+</style>
--- src/components/Experiences.vue
+++ src/components/Experiences.vue
@@ -3,6 +3,7 @@
 import resume from "../assets/resume_eng.json";
 
 import toDoubleDimensionArray from '../utils/data-to-arrays'
+import ExperienceCard from "./ExperienceCard.vue";
 
 console.log(resume);
 const experiencesArray = toDoubleDimensionArray(resume.experiences);
@@ -13,18 +14,12 @@ const experiencesArray = toDoubleDimensionArray(resume.experiences);
     <v-row>
       <v-col id="leftColumn" class="leftColumn">
         <template v-for="exp in experiencesArray[0]">
-          <div class="mb-20">
-            <h1 class="title title-left text-h4">{{ exp.title }}</h1>
-            <p class="leftParagraph font-weight-light">{{ exp.description }}</p>
-          </div>
+         <ExperienceCard :experience="exp" :is-side-left="true"/>
         </template>
       </v-col>
       <v-col id="rightColumn" class="rightColumn">
         <template v-for="exp in experiencesArray[1]">
-          <div class="mb-20">
-            <h1 class="title title-right text-h4">{{ exp.title }}</h1>
-            <p class="rightParagraph font-weight-light">{{ exp.description }}</p>
-          </div>
+          <ExperienceCard :experience="exp" :is-side-left="false"/>
         </template>
       </v-col>
     </v-row>
@@ -33,44 +28,16 @@ const experiencesArray = toDoubleDimensionArray(resume.experiences);
 
 <style scoped>
 @media (min-width: 1024px) {
-  .title::before {
-    content: "";
-    display: block;
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    background-color: black;
-  }
-
-  .title-left::before {
-    float: right;
-    margin-top: 0.4em;
-    margin-right: -0.77em;
-    margin-left: 1em;
-  }
-
-  .title-right::before {
-    float: left;
-    margin-right: 10px;
-    margin-top: 0.4em;
-    margin-left: -0.77em;
-  }
 
   .leftColumn {
     border-right: 1px solid;
+    border-color: #323b4c;
     text-align: right;
   }
 
   .rightColumn {
-    padding-top: 12.5em;
+    padding-top: 10em;
   }
 
-  .leftParagraph {
-    margin-right: 1.6em;
-  }
-
-  .rightParagraph {
-    margin-left: 0.5em;
-  }
 }
 </style>
