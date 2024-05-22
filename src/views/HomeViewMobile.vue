387aaad40501a87bbc00296f1a63336f91f73447
Soledad <soledadcot@gmail.com>
2024-05-22 15:08:41 +0200

    fix: build crossed tab + centered text & chevron front page

--- src/views/HomeViewMobile.vue
+++ src/views/HomeViewMobile.vue
@@ -5,18 +5,18 @@
 <template>
     <v-container class="scroll-container" >
         <v-row class="scroll-item" justify="center">
-            <v-col cols="12" class="mt-5 text-center">
-                <h3 class="text-h10 font-weight-light">Hi. I'm Soledad.</h3>
+            <v-col cols="12" class="mt-5 text-center mb-n16 pt-16">
+                <h3 class="text-h10 font-weight-light"> Hi. I'm Soledad.</h3>
             </v-col>
             <v-col cols="10">
                 <v-sheet class="">
                     <v-img class="rounded-card" cover src="homeview_profile.jpg"></v-img>
                 </v-sheet>
             </v-col>
-            <v-col cols="12" class="mt-6 text-center">
+            <v-col cols="12" class="mt-6 text-center mt-n16">
                 <h3 class="mb-8 text-h10 font-weight-light">How would you like to know me ?</h3>
             </v-col>
-            <v-col class="text-center">
+            <v-col class="text-center mt-n16 pb-16">
                 <font-awesome-icon :icon="['fas', 'chevron-up']" />
                 <!-- <font-awesome-icon :icon="['fas', 'chevron-down']" /> -->
             </v-col>
@@ -32,11 +32,10 @@
                 <v-btn href="https://github.com/SaladeDePoisChiche" target="_blank" variant="text">Github</v-btn>
             </div>
             <div>
-                <v-btn variant="text text-decoration-line-through">
+                <v-btn variant="text" class="text-decoration-line-through">
                     Know Me Personally
                 </v-btn>
                 <v-img cover src="sorry_not_ready-mediumdarkpink.png"></v-img>
-
             </div>
             <div>
 
