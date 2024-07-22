7505b2474690b7b3cda1ae5b242602653445f2f6
Soledad <soledadcot@gmail.com>
2024-07-22 10:55:51 +0200

    deployment

---
+++ assets/PersonalView-13e50ad9.js
@@ -0,0 +1,1 @@
+import{a6 as e,U as s,$ as o,Y as t}from"./index-1eee223e.js";const _={},a={class:"about"},n=t("h1",null,"This is my awesome page",-1),c=[n];function r(i,l){return s(),o("div",a,c)}const u=e(_,[["render",r]]);export{u as default};
